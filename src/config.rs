46397513a166cefc9d2671e5352943a08e294133
dooooooge <exat500g@gmail.com>
2025-11-07 17:50:52 +0800

    asdf

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.exat500g.top"];
+pub const RS_PUB_KEY: &str = "IF4ZnXIxrrxtw0qucwLuV+e3u7jKPzJxLAFmmYgBRAk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
